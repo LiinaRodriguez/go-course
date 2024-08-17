fb0e9649a7603ea5def026029fbb9ce1949e1f43
LinaRodriguez <lsrodriguez@unillanos.edu.co>
2024-08-17 12:08:42 -0500

    variables

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"go-course/week02"
 )
 
 func main() {
 
 	s := "gopher"
 	fmt.Println("Hello and welcome, %s!", s)
+
+	week02.Variables()
 }
--- week02/main.go
+++ week02/main.go
@@ -4,6 +4,33 @@ import (
 	"fmt"
 )
 
-func main() {
+func Variables() {
+	// Hello world
+	fmt.Println(".......week02......")
 	fmt.Println("Hello World")
+	// Variables
+	// Explicit type declaration
+	var var1 string = "Hello world"
+	//Implicit
+	var2 := "Hi"
+
+	fmt.Println(var1, var2)
+
+	//Just declaration
+	var (
+		var3 string
+		var4 int
+		var5 bool
+	)
+	fmt.Println(var3, var4, var5) //<- no initialized variables
+
+	// Formated print
+	fmt.Printf("%T\n", var3) //<- print type
+	fmt.Printf("%v\n", var3) // <- print value
+
+	var boolean bool
+	var float float32
+	var cad string
+
+	fmt.Println(boolean, float, cad)
 }
